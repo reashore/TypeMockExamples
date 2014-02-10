776c876065e5babf31a58313b277260ec7f2ff7a
Frank J. Reashore <reashore@telus.net>
2014-02-10 10:08:16 -0800

    cleanup

--- TypeMockExamples/TypeMockUnitTests/RefOutArguments.cs
+++ TypeMockExamples/TypeMockUnitTests/RefOutArguments.cs
@@ -16,29 +16,33 @@ namespace TypeMockExamples.TypeMockUnitTests.RefOutArguments
         [TestMethod]
         public void ReturnValuesInRefArgument()
         {
+            // arrange
             string outStr = "typemock";
             List<int> outList = new List<int> {100};
-
             Dependency realDependency = new Dependency();
-            Isolate.WhenCalled(() => realDependency.SomeMethod(ref outStr, out outList))
-                .IgnoreCall();
-
+            Isolate.WhenCalled(() => realDependency.SomeMethod(ref outStr, out outList)).IgnoreCall();
             ClassUnderTest classUnderTest = new ClassUnderTest();
+
+            // act
             string result = classUnderTest.GetString(realDependency);
+
+            // assert
             Assert.AreEqual("typemock1", result);
         }
 
         [TestMethod]
         public void VerifyRefArguments()
         {
+            // arrange
             string outStr = "typemock";
-
             Dependency fake = new Dependency();
             Isolate.WhenCalled(() => fake.SomeMethod(ref outStr)).IgnoreCall();
-
             ClassUnderTest classUnderTest = new ClassUnderTest();
-            string result = classUnderTest.UseRef(fake);
 
+            // act
+            classUnderTest.UseRef(fake);
+
+            // assert
             string inputShouldbe = "unit testing";
             Isolate.Verify.WasCalledWithExactArguments(() => fake.SomeMethod(ref inputShouldbe));
         }
